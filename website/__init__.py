67639e01480ae7b9fb9211e8adba0d8d559f0c6a
Oskar Garlinski <oskargarlinski@gmail.com>
2025-03-24 11:35:56 +0000

    Starting work on base template

---
+++ website/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'ANGRY CHIHUAHUA'
+
+    return app
